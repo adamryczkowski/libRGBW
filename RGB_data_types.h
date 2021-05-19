d22d2e9599d28bf646c84136dc15efa0d14b62cf
Adam Ryczkowski <adam@statystyka.net>
2021-05-19 13:23:49 +0200

    Adds convenient conversion to/from uint32_t for RGB and RGBW types plus overloaded arithmetic operators

--- RGB_data_types.h
+++ RGB_data_types.h
@@ -1,21 +1,70 @@
 #pragma once
 
+#include "Arduino.h"
+
 struct RGB { //units that are going to be projected on the LEDS directly if there were only RGB LEDS. That's why the maximal intensity can be bigger than physically possible on a LED
-	float R;
-	float G;
-	float B;
+    constexpr RGB(uint32_t c) : R(static_cast<uint8_t>(c >> 16)),
+                                G(static_cast<uint8_t>(c >> 8)),
+                                B(static_cast<uint8_t>(c)) {}
+
+    constexpr RGB(float r, float g, float b) : R(r), G(g), B(b) {}
+
+    float R;
+    float G;
+    float B;
+
+    operator uint32_t() {
+        return ((static_cast<uint32_t>(R) << 16) |
+                (static_cast<uint32_t>(G) << 8) | static_cast<uint8_t>(B));
+    }
 };
 
 struct RGBW { //units that are going to be projected on the LEDS directly
-	float R;
-	float G;
-	float B;
-	float W;
+    constexpr RGBW(uint32_t c) : R(static_cast<uint8_t>(c >> 16)),
+                                 G(static_cast<uint8_t>(c >> 8)),
+                                 B(static_cast<uint8_t>(c)),
+                                 W(static_cast<uint8_t>(c >> 24)) {}
+
+    constexpr RGBW(float r, float g, float b, float w) : R(r), G(g), B(b), W(w) {}
+
+    float R;
+    float G;
+    float B;
+    float W;
+
+    operator uint32_t() {
+        return (static_cast<uint32_t>(W) << 24) | (static_cast<uint32_t>(R) << 16) |
+               (static_cast<uint32_t>(G) << 8) | static_cast<uint8_t>(B);
+
+    }
 };
 
 struct perceptualRGB { //Units that are scaled 0..1.0. 100% of each mean a neutral white, as defined in the Neutral member.
-	float pR;
-	float pG;
-	float pB;
+    constexpr perceptualRGB() : pR(0), pG(0), pB(0) {}
+
+    perceptualRGB(float r, float g, float b) : pR(r), pG(g), pB(b) {}
+
+    float pR;
+    float pG;
+    float pB;
 };
 
+perceptualRGB operator*(const perceptualRGB &rgb1, const float f) {
+    return perceptualRGB{rgb1.pR * f,
+                         rgb1.pG * f,
+                         rgb1.pB * f};
+}
+
+
+perceptualRGB operator+(const perceptualRGB &rgb1, const perceptualRGB &rgb2) {
+    return perceptualRGB{rgb1.pR + rgb2.pR,
+                         rgb1.pG + rgb2.pG,
+                         rgb1.pB + rgb2.pB};
+}
+
+perceptualRGB operator+=(perceptualRGB rgb1, perceptualRGB rgb2) {
+    rgb1.pR += rgb2.pR;
+    rgb1.pG += rgb2.pG;
+    rgb1.pB += rgb2.pB;
+    return rgb1;
+}
